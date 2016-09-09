5b1593b88e62101971cf0fdaf4325978b5057be1
Janur Jangaraev <jjangaraev@intelliants.com>
2016-09-09 15:00:39 +0600

    Bugfix

--- includes/classes/ia.admin.categ.php
+++ includes/classes/ia.admin.categ.php
@@ -47,6 +47,53 @@ class iaCateg extends abstractDirectoryPackageAdmin
 		return iaDb::printf($this->_urlPatterns[$action], $data);
 	}
 
+	public function gridRead($params, $columns, array $filterParams = array(), array $persistentConditions = array())
+	{
+		$params || $params = array();
+
+		$start = isset($params['start']) ? (int)$params['start'] : 0;
+		$limit = isset($params['limit']) ? (int)$params['limit'] : 15;
+
+		$sort = $params['sort'];
+		$dir = in_array($params['dir'], array(iaDb::ORDER_ASC, iaDb::ORDER_DESC)) ? $params['dir'] : iaDb::ORDER_ASC;
+		$order = ($sort && $dir) ? " ORDER BY `{$sort}` {$dir}" : '';
+
+		$where = $values = array();
+		foreach ($filterParams as $name => $type)
+		{
+			if (isset($params[$name]) && $params[$name])
+			{
+				$value = iaSanitize::sql($params[$name]);
+
+				switch ($type)
+				{
+					case 'equal':
+						$where[] = sprintf('`%s` = :%s', $name, $name);
+						$values[$name] = $value;
+						break;
+					case 'like':
+						$where[] = sprintf('`%s` LIKE :%s', $name, $name);
+						$values[$name] = '%' . $value . '%';
+				}
+			}
+		}
+
+		$where = array_merge($where, $persistentConditions);
+		$where || $where[] = iaDb::EMPTY_CONDITION;
+		$where = implode(' AND ', $where);
+		$this->iaDb->bind($where, $values);
+
+		if (is_array($columns))
+		{
+			$columns = array_merge(array('id', 'update' => 1, 'delete' => 1), $columns);
+		}
+
+		return array(
+			'data' => $this->iaDb->all($columns, $where . $order, $start, $limit),
+			'total' => (int)$this->iaDb->one(iaDb::STMT_COUNT_ROWS, $where)
+		);
+	}
+
 	public function getSitemapEntries()
 	{
 		$result = array();
