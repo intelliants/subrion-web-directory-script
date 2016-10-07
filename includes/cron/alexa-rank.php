c970409719ad0af87f4b04e4e729979f59da2970
Vasily Bezruchkin <vbezruchkin@intelliants.com>
2016-10-07 14:16:56 +0600

    Alexa minor improvements.

--- includes/cron/alexa-rank.php
+++ includes/cron/alexa-rank.php
@@ -1,34 +1,9 @@
 <?php
-/******************************************************************************
- *
- * Subrion - open source content management system
- * Copyright (C) 2016 Intelliants, LLC <http://www.intelliants.com>
- *
- * This file is part of Subrion.
- *
- * Subrion is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Subrion is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Subrion. If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * @link http://www.subrion.org/
- *
- ******************************************************************************/
+//##copyright##
 
 $iaListing = $iaCore->factoryPackage('listing', 'directory');
 
-$listings = $iaDb->all(array('id', 'domain'), null, 0, null, iaListing::getTable());
-
-if ($listings)
+if ($listings = $iaDb->all(array('id', 'domain', 'alexa_rank'), "`domain` != ''", 0, null, iaListing::getTable()))
 {
 	include_once IA_PACKAGES . 'directory' . IA_DS . 'includes' . IA_DS . 'alexarank.inc.php';
 
@@ -38,7 +13,10 @@ if ($listings)
 	{
 		if ($alexaData = $iaAlexaRank->getAlexa($row['domain']))
 		{
-			$iaListing->update(array('alexa_rank' => $alexaData['rank'], 'id' => $row['id']));
+			if ($alexaData['rank'] != $row['alexa_rank'])
+			{
+				$iaCore->iaDb->update(array('alexa_rank' => $alexaData['rank'], 'id' => $row['id']), null, null, iaListing::getTable());
+			}
 		}
 	}
 }
